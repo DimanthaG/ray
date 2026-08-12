e265fcb3b05e43b791bbbb7df00ea4dd579c7388
costa-tech <w2084796@westminster.ac.uk>
2026-08-12 09:19:28 +0530

    Revert "Merge branch 'Test-' of https://github.com/DimanthaG/ray into Test-"
    This reverts commit 341556a315200f8c9110cf42605f2aaf9236abdc, reversing
    changes made to dc3d21e41f671bcfb4207ed52a8593b78433466c.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Do not add /portfolio -> /programs (browsers cache 308 for a long time).
@@ -43,50 +42,4 @@ const nextConfig = {
   },
 }
 
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Do not add /portfolio -> /programs (browsers cache 308 for a long time).
-  // If old bookmarks hit /programs, send them to the real page:
-  async redirects() {
-    return [
-      {
-        source: "/programs",
-        destination: "/portfolio",
-        permanent: false,
-      },
-    ]
-  },
-  images: {
-    domains: [
-      "images.unsplash.com",
-      "lh3.googleusercontent.com",
-      "res.cloudinary.com",
-      "i.imgur.com",
-      "flagcdn.com"
-    ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ]
-  },
-}
-
->>>>>>> a203ad2a969eb7270ca3a6c54e485115086f3c1c
 module.exports = nextConfig
